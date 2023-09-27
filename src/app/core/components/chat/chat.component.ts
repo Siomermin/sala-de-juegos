315dc76450ccb5475701521c5e9040fcd515b409
Siomermin <fermingonzalez011@gmail.com>
2023-09-26 23:46:49 -0300

    Chat v3

--- src/app/core/components/chat/chat.component.ts
+++ src/app/core/components/chat/chat.component.ts
@@ -75,27 +75,59 @@ export class ChatComponent {
     const observable = this.firestore.get('chat');
 
     observable.subscribe((res) => {
-      console.log(res);
+      const updatedMessageList: message[] = [];
+
       res.forEach((msg) => {
-        if(this.messageList.indexOf(<message>msg) == -1)
-        {
-          this.messageList.push(<message>msg);
-        }
+        const timestamp = msg['date']; // Assuming 'date' is a Firestore Timestamp
+
+        // Convert Firestore Timestamp to JavaScript Date
+        const date = new Date(
+          timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000
+        );
+
+        msg['date'] = date; // Store the date object in the 'date' field for sorting
+        updatedMessageList.push(<message>msg);
       });
-      this.messageList.sort(
-        (a, b) => Number.parseInt(a.date) - Number.parseInt(b.date)
-      );
+
+      // Sort the messages by date in descending order (newest first)
+      updatedMessageList.sort((a, b) => {
+        const dateA: any = a['date'];
+        const dateB: any = b['date'];
+
+        return dateB.getTime() - dateA.getTime(); // Compare by milliseconds (including seconds)
+      });
+
+      // Now that the messages are sorted, format the date and time
+      updatedMessageList.forEach((msg) => {
+        const date: any = msg['date'];
+
+        // Format the date as desired
+        const formattedDate = `${date.getDate()}/${(date.getMonth() + 1)
+          .toString()
+          .padStart(2, '0')} ${date
+          .getHours()
+          .toString()
+          .padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
+
+        msg['date'] = formattedDate;
+      });
+
+      this.messageList = updatedMessageList; // Update the messageList with the sorted and formatted messages
     });
   }
-
+  
   // Scrollea hasta el ultimo msj asi se muestra.
   scrollToTheLastElement() {
     let elements = document.getElementsByClassName('msj');
-    let lastElement: any = elements[elements.length - 1];
-    let toppos = lastElement.offsetTop;
+    if (elements.length > 0) {
+      let lastElement: any = elements[elements.length - elements.length + 1];
+      let toppos = lastElement.offsetTop;
 
-    let messageContainer = document.getElementById('message-container');
+      let messageContainer = document.getElementById('message-container');
 
-    messageContainer!.scrollTop = toppos;
+      if (messageContainer) {
+        messageContainer.scrollTop = toppos;
+      }
+    }
   }
 }
