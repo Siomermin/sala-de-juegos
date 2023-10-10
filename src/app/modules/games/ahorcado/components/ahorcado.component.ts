ff695a3aa7ca71e5a032ab1355994854dfec204d
Siomermin <fermingonzalez011@gmail.com>
2023-10-10 14:48:14 -0300

    Refactorizacion del ahorcado

--- src/app/modules/games/ahorcado/components/ahorcado.component.ts
+++ src/app/modules/games/ahorcado/components/ahorcado.component.ts
@@ -10,51 +10,18 @@ import { PokemonService } from '../services/pokemon.service';
 })
 export class AhorcadoComponent implements OnInit {
 
-  maxWrong: number = 6;
-
+  maxMistakes: number = 6;
   answer: string = '';
-
   mistakes: number = 0;
-
-  guessed: any[] = [];
-
-  keyboard: any[] = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-    '-'
+  guessed: string[] = [];
+  keyboard: string[] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '-'
   ];
-
   pokemones: string[] = [];
+  wordStatus: string = '';
 
-  wordStatus: any = null;
-
-  constructor(private pokemonService: PokemonService) {
-
-  }
+  constructor(private pokemonService: PokemonService) {}
 
   ngOnInit(): void {
     this.getPokemonNames();
@@ -65,117 +32,60 @@ export class AhorcadoComponent implements OnInit {
   }
 
   getPokemonNames() {
-    this.pokemonService.getAllPokemonNames().subscribe((data: any) => {
-      this.pokemones = data.results.map((pokemon: any) => pokemon.name);
-    });
+    this.pokemonService.getAllPokemonNames().subscribe(
+      (data: any) => {
+        this.pokemones = data.results.map((pokemon: any) => pokemon.name);
+      },
+      (error) => {
+        console.error('Error al traer los nombres de los Pokemones', error);
+      }
+    );
   }
 
-
   randomWord() {
-    this.answer =
-      this.pokemones[Math.floor(Math.random() * this.pokemones.length)];
+    this.answer = this.pokemones[Math.floor(Math.random() * this.pokemones.length)];
     console.log(this.answer);
   }
 
   handleGuess(chosenLetter: string) {
-    this.guessed.indexOf(chosenLetter) === -1
-      ? this.guessed.push(chosenLetter)
-      : null;
-    (<HTMLInputElement>document.getElementById(chosenLetter)).setAttribute(
-      'disabled',
-      'true'
-    );
-
-    if (this.answer.indexOf(chosenLetter) >= 0) {
-      this.guessedWord();
-      this.checkIfGameWon();
-    } else if (this.answer.indexOf(chosenLetter) === -1) {
-      this.mistakes++;
-      this.updateMistakes();
-      this.checkIfGameLost();
+    if (!this.guessed.includes(chosenLetter)) {
+      this.guessed.push(chosenLetter);
+      if (this.answer.includes(chosenLetter)) {
+        this.guessedWord();
+        this.checkIfGameWon();
+      } else {
+        this.mistakes++;
+        this.checkIfGameLost();
+      }
     }
   }
 
   checkIfGameWon() {
     if (this.wordStatus == this.answer) {
       Swal.fire('GANASTE!!!', '', 'success');
-      this.addScore();
     }
   }
 
-  addScore() {
-    let puntaje = {
-      puntos: 0,
-      uid: '',
-      fecha: '',
-    };
-    // this.authService.isLoggedIn().subscribe(usuario=>{
-    //   puntaje.uid=usuario!.uid;
-    //   puntaje.puntos=6-this.mistakes;
-    //   puntaje.fecha=Date.now().toString();
-    //   console.log(puntaje);
-    //   this.authS.agregarPuntaje(puntaje);
-    // })
-  }
   checkIfGameLost() {
-    if (this.mistakes == this.maxWrong) {
-      (<HTMLInputElement>document.getElementById('wordSpotlight')).innerHTML =
-        'La respuesta correcta era: ' + this.answer;
+    if (this.mistakes == this.maxMistakes) {
       Swal.fire('Perdiste :(', '', 'error');
     }
   }
+
   reset() {
     this.mistakes = 0;
     this.guessed = [];
-    this.keyboard.forEach((element) => {
-      (<HTMLInputElement>document.getElementById(element)).removeAttribute(
-        'disabled'
-      );
-    });
-    this.keyboard = [
-      'a',
-      'b',
-      'c',
-      'd',
-      'e',
-      'f',
-      'g',
-      'h',
-      'i',
-      'j',
-      'k',
-      'l',
-      'm',
-      'n',
-      'o',
-      'p',
-      'q',
-      'r',
-      's',
-      't',
-      'u',
-      'v',
-      'w',
-      'x',
-      'y',
-      'z',
-    ];
+    this.keyboard = 'abcdefghijklmnopqrstuvwxyz-'.split('');
     this.randomWord();
     this.guessedWord();
-    this.updateMistakes();
   }
 
   guessedWord() {
     this.wordStatus = this.answer
       .split('')
-      .map((letter) => (this.guessed.indexOf(letter) >= 0 ? letter : ' _ '))
+      .map((letter) => (this.guessed.includes(letter) ? letter : ' _ '))
       .join('');
-    (<HTMLInputElement>document.getElementById('wordSpotlight')).innerHTML =
-      this.wordStatus;
   }
 
-  updateMistakes() {
-    (<HTMLInputElement>document.getElementById('mistakes')).innerHTML =
-      this.mistakes.toString();
-  }
+
 }
